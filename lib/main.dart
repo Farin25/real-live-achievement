ccfb046d80066872f20d2736ea6e2c4d228799d0
Farin25 <204501139+Farin25@users.noreply.github.com>
2026-02-21 00:51:02 +0100

    Added Licenses page and The About/Info Page add dark theme and a variabels file for change Verion and website usw easir

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:real_live_achievments/about.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'licenses.dart';
 
+void main () {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.light,
+  ),
+),
+
+darkTheme: ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ),
+),
+  home: const AboutPage(),
+);
+  }
+}
---
+++ lib/variabels.dart
@@ -0,0 +1,7 @@
+class AppConfig {
+  static const String appName = "Mark UP";
+  static const String Version = "BETA 0.5(dev)";
+  static const String website = "farin-langner.de";
+
+
+}
