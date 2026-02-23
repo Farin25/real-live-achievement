818fde46b838b0e97dc1aace844fd2291808048b
Farin25 <204501139+Farin25@users.noreply.github.com>
2026-02-23 19:36:43 +0100

    fix the docosaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://farin-langner.de',
+  url: 'https://farin25.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/real-live-achievement/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Farin Labs', // Usually your GitHub org/user name.
-  projectName: 'Mark Up', // Usually your repo name.
+  organizationName: 'Farin25,' // Usually your GitHub org/user name.
+  projectName: 'real-live-achievement', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
