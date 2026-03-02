5cb042e796790385811834a5bec241130cbb2882
Farin25 <204501139+Farin25@users.noreply.github.com>
2026-03-02 13:57:10 +0100

    about site überarbeitet:
    added
    -Dankesagung
    -Links
    
    geändert:
    - neue anordnung
    - überschriften neu

--- lib/about.dart
+++ lib/about.dart
@@ -72,9 +72,71 @@ class AboutPage extends StatelessWidget { // Stateless widget weil braucht keine
             SizedBox(height: 30),
             //Lädt das Viedeo aus der VimeoVideo Klasse
             VimeoVideo(),
- 
-            
-            // Devs
+
+            SizedBox(height: 30),
+
+
+              Text(
+                'Links:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              linkTile('Webseite', 'https://farin25.github.io/real-live-achievement/'),
+              linkTile('SourceCode', 'https://github.com/Farin25/real-live-achievement'),
+              linkTile('Eigenes Achievment Einreichen', 'https://farin25.github.io/real-live-achievement/docs/Dein_Achievment/'),
+              
+              SizedBox(height: 10),
+
+              Text(
+                'Rechtliches:',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              linkTile('Impressum', 'https://farin25.github.io/real-live-achievement/docs/Rechtliches/impressum/'),
+              linkTile('Datenschutzerklärung', 'https://github.com/Farin25/real-live-achievement'),
+              linkTile('Algemeine Gschäfts Bedingungen', 'http://localhost:3000/real-live-achievement/docs/Rechtliches/agb'),
+              linkTile('FAQ', 'https://farin25.github.io/real-live-achievement/docs/FAQ'),
+
+              //MAil
+              linkTile(
+                'Support kontaktieren oder Fehler melden',
+                'mailto:Achievments@holzideen.org?subject=Support%20RealLiveAchievement&body=Hallo,%0A%0Aich%20habe%20folgendes%20Problem:%0A',
+              ),
+
+
+              SizedBox(height: 10),
+
+                  Text(
+              'Dankesagung:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+
+            RichText(text: TextSpan(
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+              children: [
+                TextSpan(
+                  text: 'Ein großer dank geht an alle tester der Beta Version und an alle Lehrer*innen die an uns Geglaubt haben und es ermöglich haben dieses Projekt im rahmen den Projekt orientiertes lernen zu machen. Wir bedanken uns auch bei allen die Eine Idee für ein Acheievment Eingereicht haben un und Feedback zur App gegeben haben. ',
+                ),
+              ],
+            )),
+
+            SizedBox(height: 20),
+
+     
             Text(
               'Entwickler / Herausgeber:',
               style: TextStyle(
@@ -82,7 +144,7 @@ class AboutPage extends StatelessWidget { // Stateless widget weil braucht keine
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             RichText(
               text: TextSpan(
                 style: TextStyle(
@@ -98,13 +160,11 @@ class AboutPage extends StatelessWidget { // Stateless widget weil braucht keine
               recognizer: TapGestureRecognizer()
                       ..onTap = () async {
             
-                        final Uri url = Uri.parse('https://farin-langner.de');
-                        await launchUrl(url, mode: LaunchMode.platformDefault);
-
-                       
+                      final Uri url = Uri.parse('https://farin-langner.de');
+                      await launchUrl(url, mode: LaunchMode.platformDefault);
           
                       },
-            ),
+                     ),
                   
                     TextSpan(
                       text: ' & ',
@@ -121,22 +181,6 @@ class AboutPage extends StatelessWidget { // Stateless widget weil braucht keine
                   ],
                 ),
               ),
-              SizedBox(height: 30),
-
-              Text(
-                'Rechtliches & Links:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              SizedBox(height: 10),
-
-              linkTile('Webseite', 'https://farin25.github.io/real-live-achievement/'),
-              linkTile('Impressum', 'https://farin25.github.io/real-live-achievement/docs/Rechtliches/impressum/'),
-              linkTile('Datenschutzerklärung', 'https://github.com/Farin25/real-live-achievement'),
-              linkTile('Algemeine GEschäfts Bedingungen', 'http://localhost:3000/real-live-achievement/docs/Rechtliches/agb'),
 
 
               SizedBox(height: 30), // Footer mit Abstand 30px
