d448d554019ff497f50f024a8d5b308b1acc8f63
Farin25 <204501139+Farin25@users.noreply.github.com>
2026-03-03 13:00:42 +0100

    fixed achievment list

--- lib/engine.dart
+++ lib/engine.dart
@@ -1,3 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+
 
 class AchievmentResult {
   final bool unlocked;
--- lib/user_data_services.dart
+++ lib/user_data_services.dart
@@ -1,30 +1,50 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
+ 
 class UserDataServices {
-static Future<int?> getUserAge() async {
-  final supabase = Supabase.instance.client;
-  final user = supabase.auth.currentUser;
 
-  if (user == null) return null;
+   
+  //----------------------------------
+  //------------- Geburstag ----------
+  ///---------------------------------
+  static Future<int?> getUserAge() async {
+    final prefs = await SharedPreferences.getInstance();
 
-  final profile = await supabase
-      .from('profiles')
-      .select('birthday')
-      .eq('id', user.id)
-      .single();
+    String? birthdateString = prefs.getString('birthdate');
 
-  final birthday = DateTime.parse(profile['birthday']);
-  final now = DateTime.now();
+     // Wenn lokal kein geburstdatum gespeichrt dann supbase fragen
+    if (birthdateString == null) {
+      final supabase = Supabase.instance.client;
+      final user = supabase.auth.currentUser;
 
-  int age = now.year - birthday.year;
+      if (user == null) return null;
 
-  if (now.month < birthday.month ||
-      (now.month == birthday.month && now.day < birthday.day)) {
-    age--;
+      final profile = await supabase
+          .from('profiles')
+          .select('birthday')
+          .eq('id', user.id)
+          .maybeSingle();
+
+      if (profile == null || profile['birthday'] == null) return null;
+      
+      birthdateString = profile['birthday'];
+      
+
+      await prefs.setString('birthdate', birthdateString!);
+    }
+     
+     // Rechtent alter aus
+    final birthday = DateTime.parse(birthdateString!);
+    final now = DateTime.now();
+
+    int age = now.year - birthday.year;
+
+    if (now.month < birthday.month ||
+        (now.month == birthday.month && now.day < birthday.day)) {
+      age--;
+    }
+
+    return age;
   }
-
-  return age;
 }
-
- }
